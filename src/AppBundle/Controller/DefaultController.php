410524fd22cc21acfa1c72fb166dc85297d41814
John J. Koniges <john@koniges.net>
2017-06-16 15:01:23 -0500

    Need default, putting back

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->redirectToRoute('app_manage_manage_ajaxregistrationlist');
+    }
+}
