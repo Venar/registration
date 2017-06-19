43b3739792214030dd4b9f8172d445ae3ff04d9c
John J. Koniges <john@koniges.net>
2017-06-18 19:42:20 -0500

    Fixing bad Confirmation Number

--- src/AppBundle/Service/Repository/RegistrationRepository.php
+++ src/AppBundle/Service/Repository/RegistrationRepository.php
@@ -71,7 +71,7 @@ class RegistrationRepository
             return;
         }
 
-        if ($registration->getConfirmationnumber() != '') {
+        if ($registration->getConfirmationnumber() == '') {
             $this->generateConfirmationNumber($registration);
         }
 
