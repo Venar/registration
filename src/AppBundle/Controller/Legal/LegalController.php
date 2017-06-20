d8b1aadd7d69b82c68f4e8bff2ad58077388513b
John J. Koniges <john@koniges.net>
2017-06-20 12:11:04 -0500

    Add privacy policy

---
+++ src/AppBundle/Controller/Legal/LegalController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller\Legal;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LegalController extends Controller
+{
+    /**
+     * @Route("/privacy")
+     */
+    public function showLogin()
+    {
+        return $this->render('legal/privacy.html.twig');
+    }
+}
