1845a6fffcec481ae3ae3048d0938ee6aca84dc8
John J. Koniges <john@koniges.net>
2018-03-18 09:00:39 -0500

    Add copyright and direction to license in file headers

--- src/AppBundle/Controller/Printing/PrintingController.php
+++ src/AppBundle/Controller/Printing/PrintingController.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyright (c) 2018. Anime Twin Cities, Inc.
+ *
+ * This project, including all of the files and their contents, is licensed under the terms of MIT License
+ *
+ * See the LICENSE file in the root of this project for details.
+ */
 
 namespace AppBundle\Controller\Printing;
 
